2d45669df16f5aa590519dd7ba50b0d9f60ae664
Drank Water <73241294+Dr4nk0@users.noreply.github.com>
2021-11-21 17:21:57 +0100

    Version 1.0

---
+++ ftx_setup.py
@@ -0,0 +1,147 @@
+import time
+import hmac
+import google_sheet
+import urllib.parse
+from requests import Request, Session, Response
+from typing import Optional, Dict, Any, List
+from decouple import config
+import datetime
+
+#Credentials initiation
+api_key = config('API_KEY')
+api_secret = config('API_SECRET')
+subaccount_name = config('SUBACCOUNT_NAME')
+
+# Function creation (GET, POST, DELETE, REQUEST and RESPONSE)
+
+class FtxClient:
+    _ENDPOINT = 'https://ftx.com/api/' #FTX API Endpoint
+
+    def __init__(self) -> None:
+        self._session = Session()
+        self._api_key = api_key
+        self._api_secret = api_secret
+        self._subaccount_name = subaccount_name
+
+    def _get(self, path: str, params: Optional[Dict[str, Any]] = None) -> Any:
+        return self._request('GET', path, params=params)
+
+    def _post(self, path: str, params: Optional[Dict[str, Any]] = None) -> Any:
+        return self._request('POST', path, json=params)
+
+    def _delete(self, path: str, params: Optional[Dict[str, Any]] = None) -> Any:
+        return self._request('DELETE', path, json=params)
+
+    def _request(self, method: str, path: str, **kwargs) -> Any:
+        request = Request(method, self._ENDPOINT + path, **kwargs)
+        self._sign_request(request)
+        response = self._session.send(request.prepare())
+        return self._process_response(response)
+
+    def _sign_request(self, request: Request) -> None:
+        ts = int(time.time() * 1000)
+        prepared = request.prepare()
+        signature_payload = f'{ts}{prepared.method}{prepared.path_url}'.encode()
+        print("\n#####################################################")
+        print("La requête APi est :", signature_payload)
+        print("#####################################################\n")
+        if prepared.body:
+            signature_payload += prepared.body
+            print("\n#####################################################")
+            print("La requête APi est :", signature_payload)
+            print("#####################################################\n")
+        signature = hmac.new(self._api_secret.encode(), signature_payload, 'sha256').hexdigest()
+        request.headers['FTX-KEY'] = self._api_key
+        request.headers['FTX-SIGN'] = signature
+        request.headers['FTX-TS'] = str(ts)
+        if self._subaccount_name:
+            request.headers['FTX-SUBACCOUNT'] = urllib.parse.quote(self._subaccount_name)
+
+    def _process_response(self, response: Response) -> Any:
+        try:
+            data = response.json()
+        except ValueError:
+            response.raise_for_status()
+            raise
+        else:
+            if not data['success']:
+                raise Exception(data['error'])
+            return data['result']
+
+#API GET Global functions
+
+    #Get wallet balances for each coins
+    def account_balance(self) -> List[dict]:
+        return self._get(f'wallet/balances')
+
+#API GET & POST convert calls functions
+
+    #Request a convert quote from x coin to y coin with k size
+    def request_quote(self, fromCoin: str, toCoin: str, size: float) -> dict:
+        return self._post('otc/quotes', 
+                                    {'fromCoin': fromCoin,
+                                     'toCoin': toCoin,
+                                     'size': size
+                                     })
+    
+    #Get quote status
+    def quote_status(self, quoteId: int) -> dict:
+        return self._get(f'otc/quotes/{quoteId}')
+
+    #Accept quote
+    def quote_accept(self, quoteId: int) -> dict:
+        return self._post(f'otc/quotes/{quoteId}/accept')
+
+###############################################################
+
+main = FtxClient()
+
+Coins_list = ['USD', 'DOGE']
+Wallet_balance = []
+
+ct = datetime.datetime.now()
+ts = ct.timestamp()
+current_time = datetime.datetime.now()
+
+#Convert function
+def ftx_convert(fromCoin, toCoin, size) :
+    request_quoteID = main.request_quote(fromCoin, toCoin, size)
+    quoteID = request_quoteID['quoteId']
+    status = main.quote_status(quoteID)
+    main.quote_accept(quoteID)
+    return status
+
+#Sort wallet balance
+def ftx_sort_wallet_balance(balance):
+    for crypto in balance:
+        if crypto['coin'] in Coins_list and crypto['availableWithoutBorrow'] > 0 :
+            return crypto['availableWithoutBorrow']
+            # in_wallet_coin = crypto['coin']
+            # Wallet_balance.append({in_wallet_coin:crypto})
+            # for coin in Wallet_balance :
+            #     for key, values in coin.items() :
+            #         if key == fromCoin :
+            #             return values['availableWithoutBorrow']  
+
+#Ask FTX to make a convert request
+def get_balance_and_convert(fromCoin,toCoin) :
+    balance = main.account_balance()
+    print (balance)
+    result = ftx_sort_wallet_balance(balance)
+    print (result)
+    final = ftx_convert(fromCoin,toCoin,size=result)
+    #print ("You just converted {0} {1} to {2} {3}".format(result,fromCoin,final['proceeds'],toCoin))
+    return result,fromCoin,final['proceeds'],toCoin
+
+def ftx_action() :
+    a,b,c,d = get_balance_and_convert(fromCoin='USD',toCoin='DOGE')
+    print(current_time, " : ",a,b,c,d)
+    google_sheet.twitter_line_update_1(a,b,c,d)
+    print ("Let's wait for 14s now...\n")
+    time.sleep(14)
+    a,b,c,d = get_balance_and_convert(fromCoin='DOGE',toCoin='USD')
+    print(current_time, " : ",a,b,c,d)
+    google_sheet.twitter_line_update_2(a,b,c,d)
+    google_sheet.line = google_sheet.line + 1
+
+
